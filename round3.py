32dca7ff11013dedab43df85319cf5b91c165cf5
James Falcon <therealfalcon@gmail.com>
2014-04-24 20:46:27 -0500

    Changing the world!

--- round3.py
+++ round3.py
@@ -51,8 +51,7 @@ changes = {
     ]
 }
 
-output = []
+output = [name+"'s changes are:\n"+''.join(["-" + line[1] + '\n' + '+' + line[0] + '\n' for line in zip(og_code, changes[name]) if line[0] != line[1]]) for name in changes]
 expected = ["tim's changes are:\n-function itrtr(a, i)\n+function iter (a, i)\n", "bwayne's changes are:\n", "jeff's changes are:\n-function iter (obj, iter)\n+function iter (a, i)\n-  iter = iter + 1\n+  i = i + 1\n-  local v = obj[iter]\n+  local v = a[i]\n-    return iter, v\n+    return i, v\n", "tom's changes are:\n-function iter(a, i)\n+function iter (a, i)\n-    i = i + 1\n+  i = i + 1\n-    local v = a[i]\n+  local v = a[i]\n-    if v then\n+  if v then\n-        return i, v\n+    return i, v\n-    end\n+  end\n"]
-
 
 assert(set(output) == set(expected))
--- round4.py
+++ round4.py
@@ -5,9 +5,11 @@ alphabetical order, IE - all combinations of cat, followed by hat, followed by
 in, etc etc.
 """
 
+from itertools import permutations,chain
+
 input = ['the', 'cat', 'in', 'some', 'hat']
 
-output = []
+output = list(chain(*[sorted([''.join(p) for p in [letter for letter in permutations(word)]]) for word in sorted(input)]))
 
 expected = ['act', 'atc', 'cat', 'cta', 'tac', 'tca', 'aht', 'ath', 'hat', 'hta', 'tah', 'tha', 'in', 'ni', 'emos', 'emso', 'eoms', 'eosm', 'esmo', 'esom', 'meos', 'meso', 'moes', 'mose', 'mseo', 'msoe', 'oems', 'oesm', 'omes', 'omse', 'osem', 'osme', 'semo', 'seom', 'smeo', 'smoe', 'soem', 'some', 'eht', 'eth', 'het', 'hte', 'teh', 'the']
 
